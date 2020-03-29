5ddda9b93abc0cf79655171a36e1dab1e3228fb1
Reza Ageng <rezaageng5@gmail.com>
2020-03-29 19:30:52 +0700

    DOM Selection part 2

--- DOM Selection/script.js
+++ DOM Selection/script.js
@@ -16,3 +16,16 @@ h1.style.fontSize = '50px';
 // document.getElementsByClassName() -> HTMLCollections
 const p1 = document.getElementsByClassName('p1');
 p1[0].innerHTML = 'Tulisan ini diubah menggunakan Javascript';
+
+// document.querySelector() -> Element
+const p4 = document.querySelector('#b p');
+p4.style.color = 'white';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'red';
+
+// document.querySelectorAll() -> Element
+const li = document.querySelectorAll('li')
+for (let i = 0; i < li.length; i++) {
+    li[i].innerHTML = 'Tulisan ini dari js';
+}
